3a57018948a7b6074e14eeafd91a15d8ec25abbc
Gale Bonker <gbonker@andrew.cmu.edu>
2017-05-30 01:14:19 -0700

    add details to post model and add unit tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -11,6 +11,9 @@ class PostTest < ActiveSupport::TestCase
   should validate_presence_of(:url)
   should validate_presence_of(:subreddit_id)
   should validate_numericality_of(:subreddit_id)
+  should validate_numericality_of(:number_of_comments)
+  should validate_numericality_of(:upvotes)
+  should validate_numericality_of(:downvotes)
 
   should allow_value("funny").for(:title)
   should allow_value("todayilearned").for(:title)
@@ -20,8 +23,10 @@ class PostTest < ActiveSupport::TestCase
 
   should allow_value("www.reddit.com").for(:url)
   should allow_value("www.reddit.com/r/funny").for(:url)
+  should_not allow_value("www.reddit.com/r/!funny").for(:url)
   should_not allow_value(nil).for(:url)
   should_not allow_value("").for(:url)
+  should_not allow_value("gale").for(:url)
 
   should allow_value(1).for(:subreddit_id)
   should allow_value(0).for(:subreddit_id)
@@ -29,5 +34,26 @@ class PostTest < ActiveSupport::TestCase
   should_not allow_value(nil).for(:subreddit_id)
   should_not allow_value("gale").for(:subreddit_id)
   should_not allow_value("").for(:subreddit_id)
+
+  should allow_value(10).for(:number_of_comments)
+  should allow_value(0).for(:number_of_comments)
+  should allow_value(2000).for(:number_of_comments)
+  should_not allow_value(nil).for(:number_of_comments)
+  should_not allow_value("gale bonker").for(:number_of_comments)
+  should_not allow_value("").for(:number_of_comments)
+
+  should allow_value(107).for(:upvotes)
+  should allow_value(0).for(:upvotes)
+  should allow_value(2000000).for(:upvotes)
+  should_not allow_value(nil).for(:upvotes)
+  should_not allow_value("gale virginia bonker").for(:upvotes)
+  should_not allow_value("").for(:upvotes)
+
+  should allow_value(107).for(:downvotes)
+  should allow_value(0).for(:downvotes)
+  should allow_value(2000000).for(:downvotes)
+  should_not allow_value(nil).for(:downvotes)
+  should_not allow_value("gale virginia bonker").for(:downvotes)
+  should_not allow_value("").for(:downvotes)
 
 end
